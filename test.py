61d89f92be0d532de7b14d58e9835e625c758480
Amos Jun-yeung Ng <me@amos.ng>
2020-10-24 17:19:31 -0700

    Add contacts

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
-f = open("demo.cpp", "a")
-name = input("Enter your name: ")
-f.write("""
-#include <iostream>
+from os import path
 
-int main() {{
-    std::cout << "Hello {0}!";
-    return 0;
-}}
-""".format(name))
-f.close()
+contact_file = "contacts.txt"
+
+if path.exists(contact_file):
+    f = open(contact_file, "r")
+    name, number = f.read().split("\t")
+    print("I got your contact info! You are {0}, your number is {1}"
+        .format(name, number))
+else:
+    f = open(contact_file, "w")
+    name = input("I don't have anything on file for you. Enter your name: ")
+    number = input("Enter your number: ")
+    f.write("""{0}\t{1}""".format(name, number))
+    f.close()
